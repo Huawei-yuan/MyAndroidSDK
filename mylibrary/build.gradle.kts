e5b176e1812727711c2d2f8cb520a42dd6d366b3
袁华伟 <huawei.yuan@melot.cn>
2025-06-14 17:55:12 +0800

    fix 编译错误

--- mylibrary/build.gradle.kts
+++ mylibrary/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
-    alias(libs.plugins.android.application)
+    id("com.android.library")
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("maven-publish")
+    id("signing")
 }
 
 android {
@@ -8,11 +10,8 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.hw.sdk.mylibrary"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -27,11 +26,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 }
 
@@ -44,3 +43,60 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"]) // 修改这里
+
+                groupId = "io.github.Huawei-yuan"
+                artifactId = "mysdk"
+                version = "1.0.0"
+
+                pom {
+                    name.set("MyAndroidSDK")
+                    description.set("A simple SDK example for Maven Central publication")
+                    url.set("https://github.com/Huawei-yuan/MyAndroidSDK")
+
+                    licenses {
+                        license {
+                            name.set("MIT License")
+                            url.set("https://opensource.org/licenses/MIT")
+                        }
+                    }
+
+                    developers {
+                        developer {
+                            id.set("Huawei-yuan")
+                            name.set("Huawei Yuan")
+                            email.set("huawei.yuan16@gmail.com")
+                        }
+                    }
+
+                    scm {
+                        connection.set("scm:git:github.com/Huawei-yuan/MyAndroidSDK.git")
+                        developerConnection.set("scm:git:ssh://github.com/Huawei-yuan/MyAndroidSDK.git")
+                        url.set("https://github.com/Huawei-yuan/MyAndroidSDK")
+                    }
+                }
+            }
+        }
+
+        repositories {
+            maven {
+                name = "sonatype"
+                url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+                credentials {
+                    username = findProperty("ossrhUsername") as String?
+                    password = findProperty("ossrhPassword") as String?
+                }
+            }
+        }
+    }
+
+    signing {
+        sign(publishing.publications["release"])
+    }
+}
+
